0c723bec5e2b888103a2c0eb869dbc12bb4090c0
Uliana <91365332+UlianaKustova@users.noreply.github.com>
2026-07-09 09:40:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vuetify({ autoImport: true }),
   ],
-  base: '/AdvProj/'// тест
+  base: '/AdvProj/',// тест
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
